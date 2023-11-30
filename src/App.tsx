464430f4249502d516581cd60217b1abd7a2e51a
Ramon Oliveira <ramon.ubt@gmail.com>
2023-11-30 12:22:31 -0300

    finalizando requisitos 1,2,3,4,5

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import './App.css';
+import Form from './components/Form';
 
 function App() {
+  const [isFormVisible, setIsFormVisible] = useState(false);
   return (
-    <div>
-      Hello World
+    <div className="main-container">
+      <h1>Gerenciador de senhas</h1>
+      {isFormVisible && <Form cancelFunction={ () => setIsFormVisible(false) } />}
+      {!isFormVisible && (
+        <button onClick={ () => setIsFormVisible(true) }>
+          Cadastrar nova senha
+        </button>
+      )}
+
     </div>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,102 @@
+import { useState } from 'react';
+import './Form.css';
+
+type FormProps = {
+  cancelFunction: () => void;
+};
+
+function Form({ cancelFunction }: FormProps) {
+  const [name, setName] = useState('');
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [url, setUrl] = useState('');
+  const [isButtonAvailable, setIsButtonAvailable] = useState(false);
+
+  const isPasswordShort = password.length >= 8;
+  const isPasswordLong = password.length <= 16;
+  const passwordContainsNumbersAndLetters = /[a-zA-Z]/.test(password) && /\d/.test(password);
+  const passwordContainsSpecialChars = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+  const validateForm = () => {
+    const isNameValid = !!name.trim();
+    const isLoginValid = !!login.trim();
+    const isPasswordValid = isPasswordShort
+      && isPasswordLong
+      && passwordContainsNumbersAndLetters
+      && passwordContainsSpecialChars;
+
+    setIsButtonAvailable(isNameValid && isLoginValid && isPasswordValid);
+  };
+
+  return (
+    <form onSubmit={ (e) => e.preventDefault() }>
+      <label htmlFor="name">Nome do serviço</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={ name }
+        onChange={ (e) => { setName(e.target.value); validateForm(); } }
+      />
+
+      <label htmlFor="login">Login</label>
+      <input
+        type="text"
+        name="login"
+        id="login"
+        value={ login }
+        onChange={ (e) => { setLogin(e.target.value); validateForm(); } }
+      />
+
+      <label htmlFor="password">Senha</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        value={ password }
+        onChange={ (e) => { setPassword(e.target.value); validateForm(); } }
+      />
+
+      <ul>
+        <li
+          className={ !isPasswordShort
+            ? 'invalid-password-check' : 'valid-password-check' }
+        >
+          Possuir 8 ou mais caracteres
+        </li>
+        <li
+          className={ !isPasswordLong
+            ? 'invalid-password-check' : 'valid-password-check' }
+        >
+          Possuir até 16 caracteres
+        </li>
+        <li
+          className={ !passwordContainsNumbersAndLetters
+            ? 'invalid-password-check' : 'valid-password-check' }
+        >
+          Possuir letras e números
+        </li>
+        <li
+          className={ !passwordContainsSpecialChars
+            ? 'invalid-password-check' : 'valid-password-check' }
+        >
+          Possuir algum caractere especial
+        </li>
+      </ul>
+
+      <label htmlFor="url">URL</label>
+      <input
+        type="text"
+        name="url"
+        id="url"
+        value={ url }
+        onChange={ (e) => setUrl(e.target.value) }
+      />
+
+      <button disabled={ !isButtonAvailable }>Cadastrar</button>
+      <button onClick={ cancelFunction }>Cancelar</button>
+    </form>
+  );
+}
+
+export default Form;
