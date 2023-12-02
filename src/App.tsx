25d0994bd7536c7adba99cd529633fb6316ae640
Ramon <ramon.ubt@gmail.com>
2023-12-02 02:30:38 -0300

    Finalizando requisitos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import './App.css';
+import Swal from 'sweetalert2';
 import Form from './components/Form';
 import Service from './components/Service';
 import { ServiceData } from './serviceType';
+import 'sweetalert2/dist/sweetalert2.css';
 
 function App() {
   const [isFormVisible, setIsFormVisible] = useState(false);
@@ -14,7 +16,41 @@ function App() {
     setServices([...services, service]);
     setIsFormVisible(false);
     setDontContainPasswords(false);
+    Swal.fire({
+      title: 'Cadastro realizado!',
+      text: 'Serviço cadastrado com sucesso',
+      timer: 1500,
+      icon: 'success',
+    });
   };
+
+  // const deleteService = (service: ServiceData) => {
+  //   Swal.fire({
+  //     title: 'Tem certeza?',
+  //     text: `Você deseja excluir o serviço ${service.name}?`,
+  //     icon: 'warning',
+  //     showCancelButton: true,
+  //     confirmButtonColor: '#3085d6',
+  //     cancelButtonColor: '#d33',
+  //     confirmButtonText: 'Sim, excluir!',
+  //     background: '#282c34',
+  //     color: '#ffffff',
+  //   }).then((result) => {
+  //     if (result.isConfirmed) {
+  //       const actualServices = services
+  //         .filter((serviceFound) => service.name !== serviceFound.name);
+  //       setServices(actualServices);
+  //       setDontContainPasswords(true);
+  //       Swal.fire({
+  //         title: 'Excluído!',
+  //         text: `O serviço ${service.name} foi excluído.`,
+  //         icon: 'success',
+  //         background: '#282c34',
+  //         color: '#ffffff',
+  //       });
+  //     }
+  //   });
+  // };
 
   const deleteService = (service: ServiceData) => {
     const actualServices = services.filter((serviceFound) => service
